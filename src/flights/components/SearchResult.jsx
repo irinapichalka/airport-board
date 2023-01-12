f4adae9137b3a6701762bfaa66e3c9851e92a727
irinapichalka <irinarescan@gmail.com>
2023-01-12 21:40:00 +0200

    dd

--- src/flights/components/SearchResult.jsx
+++ src/flights/components/SearchResult.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState, forwardRef } from "react";
 import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
+import DatePicker from "react-datepicker";
+//import "react-datepicker/dist/react-datepicker.css";
 import Arrival from "./Arrival";
 import Departure from "./Departure";
 import DepartureSVG from "../../svg/DepartureSVG";
 import ArrivalSVG from "../../svg/ArrivalSVG";
 
 const SearchResult = () => {
+  const [startDate, setStartDate] = useState(new Date());
+  const ExampleCustomInput = forwardRef(({ value, onClick }, ref) => (
+    <span
+      className="search-result__date-num show-calendar example-custom-input"
+      type="date"
+      onClick={onClick}
+      ref={ref}
+    >
+      {value}
+    </span>
+  ));
   return (
     <BrowserRouter>
       <div className="search-result">
@@ -29,12 +42,13 @@ const SearchResult = () => {
       </div>
       <div className="search-result__dates">
         <div className="search-result__dates-calendar">
-          <span
-            className="search-result__date-num show-calendar example-custom-input"
-            type="date"
-          >
-            12/01
-          </span>
+          <div>
+            <DatePicker
+              selected={startDate}
+              onChange={(date) => setStartDate(date)}
+              customInput={<ExampleCustomInput />}
+            />
+          </div>
         </div>
         <div className="search-result__dates-conteiner">
           <div className="search-result__date yesterday active-date">
